84c387c6a618d8ae27918f7a9c8c75436ce38b40
devv77 <lencsesdavid@gmail.com>
2022-03-15 12:40:35 +0100

    react router back to 5.2 and routing is works now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import  { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-
 import './App.css';
+
+const HatsPage =()=>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div >
-     <HomePage/>
-    </div>
+    //<Router>
+      <Switch>
+        <Route exact path='/' component={HomePage }></Route>
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
+    //</Router>
   );
 }
 
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
-const HomePage =() =>(
-  <div className='homePage'>
-      <div className='directory-menu'>          
-          <Directory></Directory>
-             
-      </div>
-  </div>)
+const HomePage = ({}) => (
+  <div className="homePage">
+    <Directory></Directory>
+  </div>
+);
 
   export default HomePage;;
